153ddc0d6e5e67f1d6779e7f1389f688ee7a7928
Alon (PC) <downtown2u@gmail.com>
2021-04-08 18:39:00 +0300

    Added token loading from storage

---
+++ etgarbot/token.py
@@ -0,0 +1,33 @@
+import logging
+
+
+VALID_TOKEN_FILE_NAMES = (
+    "token", "token.txt",
+    "credential", "credential.txt",
+    "credentials", "credentials.txt",
+)
+
+
+def get_token():
+    """ Checks if a credentials / config file is provided.
+    If such file is not located, requests the token from the user
+    directly using the console. """
+
+    for filename in VALID_TOKEN_FILE_NAMES:
+
+        try:
+            with open(filename, 'r') as file:
+                token = file.read().strip()
+                logging.info(
+                    "Loaded discord bot token from the '%s' file", filename)
+                return token
+
+        except FileNotFoundError:
+            # If current file name is not found,
+            # continue to check other file names
+            continue
+
+    # If could not find a file for the token, returns None.
+    logging.error(
+        "A file with a discord token (e.g. 'token.txt') wasn't located.")
+    return None
