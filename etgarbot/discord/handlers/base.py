2c0fdef68b1849d71a370b8d40650e7087cd66b6
Alon (PC) <downtown2u@gmail.com>
2021-04-10 14:29:50 +0300

    🐛 Converted 'message_to_score' to regular sync function

--- etgarbot/discord/handlers/base.py
+++ etgarbot/discord/handlers/base.py
@@ -9,7 +9,11 @@ MessageScore = typing.Union[float, int, ]
 
 class BaseMessageHandler(ABC):
 
-    async def message_to_score(self, message: discord.Message) -> MessageScore:
+    async def message_handle(self, message: discord.Message) -> None:
+        """ Recives a message instance and handles it (for example, with
+        another response message from the bot). """
+
+    def message_to_score(self, message: discord.Message) -> MessageScore:
         """ Recives a message instance, and returns a score between 0 and 1.
         When the score is 1 (integer), it is guaranteed that the
         `message_handle` method will be called with the given message.
@@ -18,10 +22,6 @@ class BaseMessageHandler(ABC):
         With any values between 0 and 1, the discord bot client will
         automatically pick the message that has the top score and will call
         `message_handle` on that instance. """
-
-    async def message_handle(self, message: discord.Message) -> None:
-        """ Recives a message instance and handles it (for example, with
-        another response message from the bot). """
 
     def short_description(self,) -> str:
         """ Returns a short description that describes the current message
